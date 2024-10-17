af51fc44f32495a3470ddcdb3e06fcf5ddb679ea
Sadanand Modak <sadanandm@utexas.edu>
2024-10-17 01:42:42 -0500

    barebones + intermed flags launchfile

--- config/navigation.lua
+++ config/navigation.lua
@@ -3,17 +3,17 @@ function deg2rad(deg)
 end
 
 NavigationParameters = {
-  laser_topic = "velodyne_2dscan_highbeams";
+  laser_topics = {
+    "/velodyne_2dscan_highbeams",
+    "/kinect_laserscan",
+  };
+  laser_frame = "base_link";
   odom_topic = "odom";
   localization_topic = "localization";
   image_topic = "/camera/rgb/image_raw/compressed";
   init_topic = "initialpose";
   enable_topic = "autonomy_arbiter/enabled";
-  laser_loc = {
-    x = 0.065;
-    y = 0;
-  };
-  dt = 0.025;
+  dt = 0.060;
   max_linear_accel = 0.5;
   max_linear_decel = 0.5;
   max_linear_speed = 1.4;
@@ -39,6 +39,23 @@ NavigationParameters = {
   target_angle_tolerance = deg2rad(20);
   use_map_speed = true;
   use_kinect = false;
+  intermediate_goal_dist = 10; 
+  max_inflation_radius = 1;
+  min_inflation_radius = 0.3;
+  local_costmap_resolution = 0.05;
+  local_costmap_size = 20;
+  global_costmap_resolution = 0.1;
+  global_costmap_size_x = 100;
+  global_costmap_size_y = 100;
+  global_costmap_origin_x = -10;
+  global_costmap_origin_y = 0;
+  lidar_range_min = 0.1;
+  lidar_range_max = 10.0;
+  replan_dist = 2;
+  object_lifespan = 15;
+  inflation_coeff = 8;
+  distance_weight = 3;
+  recovery_carrot_dist = 0.7;
 };
 
 AckermannSampler = {
---
+++ launch/ldos_setgoal.sh
@@ -0,0 +1,16 @@
+rostopic pub --once /move_base_simple/goal geometry_msgs/PoseStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+pose:
+  position:
+    x: 6.33
+    y: 72.33
+    z: 0
+  orientation:
+    x: 0
+    y: 0
+    z: 0
+    w: 1.0"
